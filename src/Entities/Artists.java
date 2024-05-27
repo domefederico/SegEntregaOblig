e1ab58eeda51f8bc8bad430978739ed77cecc719
JuanFolle2004 <jfollesoler@gmail.com>
2024-05-27 12:17:20 -0300

    devolve

--- src/Entities/Artists.java
+++ src/Entities/Artists.java
@@ -2,4 +2,23 @@ package Entities;
 
 public class Artists {
     // H
+
+
+    public String devolve(int num, String linea) {          // num es 1 si es id, 2 si es nombre
+        int o = 0;
+        for (char c : linea.toCharArray()) {
+            if (c == '"') {
+                o++;
+            }
+            if (o == 2*num - 1) {
+
+            }
+        }
+
+
+        return null;
+    }
+
+    // id = devolve(1,datos(i))
+    // nombre = devolve(2,datos(i))
 }
