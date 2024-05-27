cf67015ce268ce0d7d32825b7b77217e03d255de
domefederico <domefederico@gmail.com>
2024-05-27 11:38:07 -0300

    empieza csv reader

--- src/ParaProbar.java
+++ src/ParaProbar.java
@@ -8,5 +8,10 @@ public class ParaProbar {
     public static void main(String[] args) throws IOException, CsvException {
         CSVReader csvReader = new CSVReader(new FileReader("data_set.csv"));
         List<String[]> datos = csvReader.readAll();
+//        for (int i = 0 ; i< datos.size() ; i++){
+//            for (int j=0 ; j< datos.get(i)[0].length();j++) {
+//                char c = datos.get(i)[0].charAt(j);
+//            }
+//        }
     }
 }
